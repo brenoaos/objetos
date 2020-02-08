234887faa8819139d4becce5ebade57d686b1e15
Breno Oliveira <brenoaos@me.com>
2020-02-08 11:17:03 -0300

    new design

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import 'hammerjs';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { MaterialModule } from './shared/material.modules';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  useJit: true,
+  preserveWhitespaces: true
+})
+  .catch(err => console.log(err));
