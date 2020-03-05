affa1159a5fa1ddd013ee94edad7b16f28fbe59f
Breno Oliveira <brenoaos@me.com>
2020-03-05 00:36:01 -0300

    formbuilder em pessoa

--- src/app/views/cadastro/pessoa/pessoa.module.ts
+++ src/app/views/cadastro/pessoa/pessoa.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { PessoaRoutingModule } from './pessoa.rounting';
 import { PessoaListComponent } from './list/pessoa.list.component';
@@ -12,6 +12,8 @@ import { HttpClientModule } from '@angular/common/http';
     CommonModule,
     PessoaRoutingModule,
     FormsModule,
+    FormBuilder,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   declarations: [
